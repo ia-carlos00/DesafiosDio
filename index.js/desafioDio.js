a2a91bf644f96a45f7773cc795cab8d6b87702a9
ia-carlos00 <iaferr72@gmail.com>
2023-12-08 22:36:00 -0300

    desafio proposto
    em resposta a um desafio que me foi proposto .

---
+++ index.js/desafioDio.js
@@ -0,0 +1,3 @@
+//Este projeto é um desafio de superação para mostrar a minha evolução de forma pregressiva!
+//Marca o inicio da minha caminhaada em linguagem de programação.
+//Mas caso pareça muito simples, peço que me perdoe, meu objetivo é evoluir e ser a minha melhor versão.
