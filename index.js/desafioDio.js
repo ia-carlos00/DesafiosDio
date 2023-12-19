450190c63fd4f9d53ca084943618d161fe1af064
ia-carlos00 <iaferr72@gmail.com>
2023-12-19 09:06:31 -0300

    calcular partida ranqueada
    fazendo  novo projeto

--- index.js/desafioDio.js
+++ index.js/desafioDio.js
@@ -1,3 +1,10 @@
-//Este projeto é um desafio de superação para mostrar a minha evolução de forma pregressiva!
-//Marca o inicio da minha caminhaada em linguagem de programação.
+//Este projeto é um desafio de superação para mostrar a minha evolução de forma pregressiva!Marca o inicio da minha caminhaada em linguagem de programação.
 //Mas caso pareça muito simples, peço que me perdoe, meu objetivo é evoluir e ser a minha melhor versão.
+//
+//calculadora de partidas rankeadas
+//
+function calcularNivelRankeadas(vitorias, derrotas) {
+    // Calcula o saldo de Rankeadas
+    const saldoVitorias = vitorias - derrotas;
+
+   
