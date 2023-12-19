1c17d66099934ea458bbb2c74f774b45d3a84439
ia-carlos00 <iaferr72@gmail.com>
2023-12-19 10:52:52 -0300

    criando um calculador de nivel
    edittando projeto

--- index.js/desafioDio.js
+++ index.js/desafioDio.js
@@ -1,10 +1,26 @@
-//Este projeto é um desafio de superação para mostrar a minha evolução de forma pregressiva!Marca o inicio da minha caminhaada em linguagem de programação.
-//Mas caso pareça muito simples, peço que me perdoe, meu objetivo é evoluir e ser a minha melhor versão.
+// Calculadora de partidas rankeadas
 //
-//calculadora de partidas rankeadas
-//
-function calcularNivelRankeadas(vitorias, derrotas) {
-    // Calcula o saldo de Rankeadas
-    const saldoVitorias = vitorias - derrotas;
+function contarVitoriaDerrota(vitoria, derrota){
+const saldodeVitoria = vitoria- derrota;
 
-   
+
+ if( nivel= vitoria < 10){
+    console.log( ' Ferro');
+
+ }else if(nivel= vitoria <= 11 && vitoria >= 20){
+    console. log(nivel=' Bronze');
+ }else if(  vitoria <= 21 && vitoria >= 50){
+    console.log( nivel=' Prata');
+ }else if( vitoria >= 51 && vitoria >= 80){
+    console.log(nivel=' Ouro');
+ } else if( vitoria <= 81 && vitoria >= 90){
+    console.log(nivel=' Diamante');
+ } else if( vitoria <= 91 && vitotia >= 100){
+    console.log(nivel=' Lendário')
+ } else {
+    console.log(nivel=' Imortal')
+ }
+// Exibindo uma mensagem 
+console.log('O Herói tem saldo de '+ saldodeVitoria, ' e está no nivél de :'+ nivel);
+contarVitoriaDerrota( 8, 15)
+}
